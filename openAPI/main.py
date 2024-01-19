923ee6497d7538b5ad4679f378f2a5e9152121b6
chia <leeechia@gmail.com>
2024-01-19 15:53:48 +0800

    modify

--- openAPI/main.py
+++ openAPI/main.py
@@ -1,13 +1,18 @@
 from typing import Union
-
 from fastapi import FastAPI
+import redis
+import os
+from dotenv import load_dotenv
+load_dotenv()
+redis_conn = redis.Redis.from_url(os.environ.get('REDIS_HOST_PASSWORD'))
 
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    counter = redis_conn.incr('test:increment',1)
+    return {"counter":counter}
 
 
 @app.get("/items/{item_id}")
