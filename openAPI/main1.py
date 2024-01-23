8d4b853be1d7bd384bb61a6f9ae18b71bfc4ac0d
Chia <leeechia@gmail.com>
2024-01-23 21:52:31 +0800

    modify

--- openAPI/main1.py
+++ openAPI/main1.py
@@ -14,6 +14,12 @@ def read_root():
     return {"Hello": "World"}
 
 @app.get("/items/{item_id}")
-async def get_item(item_id):
+async def get_item(item_id:int):
     print(f"使用者輸入了:{item_id}")
     return {"item_id": item_id}
+
+@app.get("/items/{date}/{celsius}")
+async def get_item(date:str,celsius:float):
+    print(f"日期:{date}")
+    print(f"溫度:{celsius}")
+    return {"日期":date,"攝氏溫度":celsius}
