d394c17f73ccec62d22c74ebeadfbcbdf920b916
chia <leeechia@gmail.com>
2024-01-26 14:16:56 +0800

    modify

--- UI/main.py
+++ UI/main.py
@@ -1,7 +1,11 @@
 import requests
 import pandas as pd
+import streamlit as st
+from streamlit_autorefresh import st_autorefresh
 
-url = 'https://openapi-test-u1jw.onrender.com/pico_w/?count=5'
+url = 'https://openapi-test-u1jw.onrender.com/pico_w/?count=10'
+#自動reload頁面每10秒
+st_autorefresh(interval=10000, limit=100, key="fizzbuzzcounter")
 
 r = requests.get(url=url)
 
@@ -9,5 +13,15 @@ if r.status_code == 200:
     print("下載成功")
     data = r.json()
 
-    dataFrame = pd.DataFrame(data)
-    print(dataFrame)
+dataFrame = pd.DataFrame(data)
+
+st.header("學院養雞場")
+st.divider()
+st.caption("溫度-光線表😍")
+st.write(dataFrame)
+st.divider()
+st.caption("光線")
+st.line_chart(dataFrame,x='date',y='light')
+st.divider()
+st.caption("溫度")
+st.line_chart(dataFrame,x='date',y='temperature',color='#ff0000')
