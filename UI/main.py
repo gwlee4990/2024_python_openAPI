f486684cce4cf60c32895d0bb62541222c1e2269
chia <leeechia@gmail.com>
2024-01-26 11:12:01 +0800

    modify

---
+++ UI/main.py
@@ -0,0 +1,13 @@
+import requests
+import pandas as pd
+
+url = 'https://openapi-test-u1jw.onrender.com/pico_w/?count=5'
+
+r = requests.get(url=url)
+
+if r.status_code == 200:
+    print("下載成功")
+    data = r.json()
+
+    dataFrame = pd.DataFrame(data)
+    print(dataFrame)
